f0177312631d25eca81ce003c23895258cd811c8
Callum McLuskey <callummcluskey100@gmail.com>
2023-02-10 18:03:25 +0000

    Delete src directory

--- src/core/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Puck } from './puck';
-import { Pixl } from './pixl';
-import { Bangle } from './bangle';
-import { DeviceController } from './device-controller';
-
-export { Puck, Pixl, Bangle, DeviceController };
-
-export default DeviceController;
